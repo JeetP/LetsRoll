c59e37d2771325d5d5c87d9c029ae58ae9ee9759
Singhal <SSINGHA5@jaguarlandrover.com>
2020-10-29 17:49:41 +0530

    complete worker implementation

--- app/src/main/java/com/example/letsroll/MainActivity.kt
+++ app/src/main/java/com/example/letsroll/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
@@ -15,29 +17,36 @@ class MainActivity : AppCompatActivity() {
     private var DiceImageResoure = -1
     lateinit var viewModel : DiceViewModel
     private val workManager = WorkManager.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var dice_button: Button = findViewById(R.id.tap_here_button)
+
+        val dice_button: Button = findViewById(R.id.tap_here_button)
+
         diceImage = findViewById(R.id.dice_image)
-        viewModel = ViewModelProviders.of(this).get(DiceViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(DiceViewModel::class.java)
         DiceImageResoure = viewModel.getImageResource()
+
         if(DiceImageResoure != -1) {
             diceImage.setImageResource(DiceImageResoure)
         }
-        dice_button.setOnClickListener() {
-            Roll()
-        }
+
+        dice_button.setOnClickListener { Roll() }
     }
 
-    public fun Roll() {
+     fun Roll() {
         val request = OneTimeWorkRequest.from(DiceWorker::class.java)
 
         workManager.enqueue(request)
-        workManager.getWorkInfoByIdLiveData(request.id).observe()
-        viewModel.rollDice()
-        DiceImageResoure = viewModel.getImageResource()
-        diceImage.setImageResource(DiceImageResoure)
+        workManager.getWorkInfoByIdLiveData(request.id).observe(this, {
+            if (it.state.isFinished) {
+                viewModel.rollDice()
+                DiceImageResoure = viewModel.getImageResource()
+                diceImage.setImageResource(DiceImageResoure)
+            }
+        })
+
     }
 
 }
